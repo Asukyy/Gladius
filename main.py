34f5a8afa758c41fb615348c85367deb9b93bb97
Mathis Boulais <mathis.oxym@gmail.com>
2023-02-16 21:18:06 +0100

    square

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+#affiche un carre vert au milieu de l'écran
+    pygame.draw.rect(window, (0, 255, 0), pygame.Rect(300, 200, 200, 200))
 
     # Mise à jour de l'affichage
     pygame.display.flip()
