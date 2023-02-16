e217030277044554b7280e43526bf8e6702ba2a6
Asukyy <bmathisdu94@gmail.com>
2023-02-16 20:24:15 +0100

    branch

--- game/main.py
+++ game/main.py
@@ -3,26 +3,19 @@ import pygame
 # Initialisation de Pygame
 pygame.init()
 
-# Création d'une fenêtre de jeu
-screen = pygame.display.set_mode((400, 400))
+# Création de la fenêtre
+window = pygame.display.set_mode((800, 600))
 
-# Définition de la couleur rouge
-red = (255, 0, 0)
-
-# Création d'un carré rouge dans la fenêtre de jeu
-rect = pygame.Rect(100, 100, 50, 50)
-pygame.draw.rect(screen, red, rect)
-
-# Rafraîchissement de la fenêtre de jeu
-pygame.display.flip()
-
-# Boucle principale de jeu
+# Boucle principale 
 running = True
 while running:
-    # Gestion des événements de la fenêtre de jeu
+    # Gestion des événements
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Mise à jour de l'affichage
+    pygame.display.flip()
 
 # Fermeture de Pygame
 pygame.quit()
